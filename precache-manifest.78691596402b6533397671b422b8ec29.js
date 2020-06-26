6ce0da94d0a207220a446a832463557c063eecea
aimnfire <61099792+aimnfire@users.noreply.github.com>
2020-06-26 09:06:42 -0500

    Updates

---
+++ precache-manifest.78691596402b6533397671b422b8ec29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78db1dbc49421a5cd40061afea228b0b",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "10e583850d30601c83fa",
+    "url": "/pomodoro-clock/static/css/main.d248baa4.chunk.css"
+  },
+  {
+    "revision": "c724dcc1bd3ce4f8bbf4",
+    "url": "/pomodoro-clock/static/js/2.feca8fb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.feca8fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10e583850d30601c83fa",
+    "url": "/pomodoro-clock/static/js/main.25681dce.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
